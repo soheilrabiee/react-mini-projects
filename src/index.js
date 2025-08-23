7fb58dcb85c8e781bda0c24a19533d8085981135
Soheil <iamsoheil.r@gmail.com>
2025-08-23 18:54:40 +0330

    Add styles to the components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
     {
@@ -48,7 +49,7 @@ const pizzaData = [
 
 function App() {
     return (
-        <div>
+        <div className="container">
             <Header />
             <Menu />
             <Footer />
@@ -57,24 +58,42 @@ function App() {
 }
 
 function Header() {
-    return <h1>Fast React Pizza Co.</h1>;
+    // inline style
+
+    // const style = {
+    //     color: "red",
+    //     fontSize: "48px",
+    //     textTransform: "uppercase",
+    // };
+
+    const style = {};
+    return (
+        <header className="header">
+            <h1 style={style}>Fast React Pizza Co.</h1>;
+        </header>
+    );
 }
 
 function Menu() {
     return (
-        <div>
+        <main className="menu">
             <h2>Our menu</h2>
             <Pizza />
             <Pizza />
             <Pizza />
             <Pizza />
-        </div>
+        </main>
     );
 }
 
 function Footer() {
+    const hour = new Date().getHours();
+    const openHour = 12;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour;
+
     return (
-        <footer>
+        <footer className="footer">
             {new Date().toLocaleTimeString()}. We're currently open!
         </footer>
     );
@@ -85,7 +104,7 @@ function Pizza() {
     return (
         <div>
             <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-            <h2>Pizza Spinaci</h2>
+            <h3>Pizza Spinaci</h3>
             <p>Tomato, mozarella, spinach, and ricotta cheese</p>
         </div>
     );
