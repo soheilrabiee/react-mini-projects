f9bb167fbc31d666d99a05fc48d47ef5c73eede6
Soheil <iamsoheil.r@gmail.com>
2025-08-26 19:55:01 +0330

    Add order component

--- src/index.js
+++ src/index.js
@@ -94,7 +94,7 @@ function Menu() {
 
 function Footer() {
     const hour = new Date().getHours();
-    const openHour = 20;
+    const openHour = 12;
     const closeHour = 22;
     const isOpen = hour >= openHour && hour <= closeHour;
 
@@ -103,13 +103,7 @@ function Footer() {
     return (
         <footer className="footer">
             {isOpen ? (
-                <div className="order">
-                    <p>
-                        We're open until {closeHour}:00. Come visit us or order
-                        online.
-                    </p>
-                    <button className="btn">Order</button>
-                </div>
+                <Order closeHour={closeHour} />
             ) : (
                 <p>
                     We're happy to welcome you between {openHour}:00 and{" "}
@@ -119,6 +113,18 @@ function Footer() {
         </footer>
     );
     // return React.createElement("footer", null, "We're currently open!");
+}
+
+function Order(props) {
+    return (
+        <div className="order">
+            <p>
+                We're open until {props.closeHour}:00. Come visit us or order
+                online.
+            </p>
+            <button className="btn">Order</button>
+        </div>
+    );
 }
 
 function Pizza(props) {
