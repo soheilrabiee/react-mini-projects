60a799e0c05e67a6dd66019d0947f035b2ca0f11
Soheil <iamsoheil.r@gmail.com>
2025-08-26 15:54:48 +0330

    Render pizza list

--- src/index.js
+++ src/index.js
@@ -78,19 +78,12 @@ function Menu() {
     return (
         <main className="menu">
             <h2>Our menu</h2>
-            <Pizza
-                name="Pizza Spinaci"
-                ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-                photoName="pizzas/spinaci.jpg"
-                price={10}
-            />
 
-            <Pizza
-                name="Pizza Funghi"
-                ingredients="Tomato, mushrooms"
-                price={12}
-                photoName="pizzas/funghi.jpg"
-            />
+            <ul className="pizzas">
+                {pizzaData.map((pizza) => (
+                    <Pizza pizzaObj={pizza} key={pizza.name} />
+                ))}
+            </ul>
         </main>
     );
 }
@@ -111,14 +104,14 @@ function Footer() {
 
 function Pizza(props) {
     return (
-        <div className="pizza">
-            <img src={props.photoName} alt={props.name} />
+        <li className="pizza">
+            <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
             <div>
-                <h3>{props.name}</h3>
-                <p>{props.ingredients}</p>
-                <span>{props.price}</span>
+                <h3>{props.pizzaObj.name}</h3>
+                <p>{props.pizzaObj.ingredients}</p>
+                <span>{props.pizzaObj.price}</span>
             </div>
-        </div>
+        </li>
     );
 }
 
