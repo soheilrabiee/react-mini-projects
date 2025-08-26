c69ad5a4ffc66029edb521226bb25af8caaf4e40
Soheil <iamsoheil.r@gmail.com>
2025-08-26 20:27:07 +0330

    Conditionally set text and class

--- src/index.js
+++ src/index.js
@@ -138,12 +138,12 @@ function Pizza({ pizzaObj }) {
     // if (props.pizzaObj.soldOut) return null;
 
     return (
-        <li className="pizza">
+        <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
             <img src={pizzaObj.photoName} alt={pizzaObj.name} />
             <div>
                 <h3>{pizzaObj.name}</h3>
                 <p>{pizzaObj.ingredients}</p>
-                <span>{pizzaObj.price}</span>
+                <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
             </div>
         </li>
     );
