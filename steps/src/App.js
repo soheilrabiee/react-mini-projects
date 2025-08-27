c60b8d864f4439da7e2db285eb7e1dcebf3db2c9
Soheil <iamsoheil.r@gmail.com>
2025-08-27 17:38:50 +0330

    Add step state variable

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
     "Learn React ⚛️",
     "Apply for jobs 💼",
@@ -5,24 +7,25 @@ const messages = [
 ];
 
 export default function App() {
-    const step = 3;
+    // step state
+    const [step, setStep] = useState(1);
 
     // using handle name shows that the function is an event handler
     // better to declare handlers outside of JSX and call them on event
     function handlePrevious() {
-        alert("Previous");
+        if (step > 1) setStep(step - 1);
     }
 
     function handleNext() {
-        alert("Next");
+        if (step < 3) setStep(step + 1);
     }
 
     return (
         <div className="steps">
             <div className="numbers">
-                <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-                <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-                <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+                <div className={step >= 1 ? "active" : ""}>1</div>
+                <div className={step >= 2 ? "active" : ""}>2</div>
+                <div className={step >= 3 ? "active" : ""}>3</div>
             </div>
 
             <p className="message">
