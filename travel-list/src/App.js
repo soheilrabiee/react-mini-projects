b4112617e5b6eb53bc033d35f23b21125f6ad85e
Soheil <iamsoheil.r@gmail.com>
2025-08-29 00:25:09 +0330

    Handle item toggling from the list of items

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -12,11 +12,23 @@ export default function App() {
         setItems((items) => items.filter((item) => item.id !== id));
     }
 
+    function handleToggleItem(id) {
+        setItems((items) =>
+            items.map((item) =>
+                item.id === id ? { ...item, packed: !item.packed } : item
+            )
+        );
+    }
+
     return (
         <div className="app">
             <Logo />
             <Form onAddItems={handleAddItems} />
-            <PackingList items={items} onDeleteItem={handleDeleteItem} />
+            <PackingList
+                items={items}
+                onDeleteItem={handleDeleteItem}
+                onToggleItem={handleToggleItem}
+            />
             <Stats />
         </div>
     );
@@ -75,7 +87,7 @@ function Form({ onAddItems }) {
     );
 }
 
-function PackingList({ items, onDeleteItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem }) {
     return (
         <div className="list">
             <ul>
@@ -83,6 +95,7 @@ function PackingList({ items, onDeleteItem }) {
                     <Item
                         item={item}
                         onDeleteItem={onDeleteItem}
+                        onToggleItem={onToggleItem}
                         key={item.id}
                     />
                 ))}
@@ -91,12 +104,18 @@ function PackingList({ items, onDeleteItem }) {
     );
 }
 
-function Item({ item, onDeleteItem }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
     return (
         <li>
+            <input
+                type="checkbox"
+                value={item.packed}
+                onChange={() => {
+                    onToggleItem(item.id);
+                }}
+            />
             <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-                {item.quantity}
-                {item.description}
+                {item.quantity} {item.description}
             </span>
             <button onClick={() => onDeleteItem(item.id)}>❌</button>
         </li>
