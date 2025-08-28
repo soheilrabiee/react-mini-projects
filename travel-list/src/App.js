646e6923501089d31dcd56e8238a8e51d155fd13
Soheil <iamsoheil.r@gmail.com>
2025-08-28 14:57:30 +0330

    Add static data to the components

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,6 +1,36 @@
-export default function App() {}
+export default function App() {
+    return (
+        <div className="app">
+            <Logo />
+            <Form />
+            <PackingList />
+            <Stats />
+        </div>
+    );
+}
 
-function Logo() {}
-function Form() {}
-function PackingList() {}
-function Stats() {}
+function Logo() {
+    return <h1>🌴 Far Away 💼</h1>;
+}
+
+function Form() {
+    return (
+        <div className="add-form">
+            <h3>What do you need for your ✈️ trip?</h3>
+        </div>
+    );
+}
+
+function PackingList() {
+    return <div className="list">List</div>;
+}
+
+function Stats() {
+    return (
+        <footer className="stats">
+            <em>
+                👜 You have X items on your list, and you already packed X (X%)
+            </em>
+        </footer>
+    );
+}
