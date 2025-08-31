2985161060c480ab6c65c95a2c583e520ea685be
Soheil <iamsoheil.r@gmail.com>
2025-08-31 15:52:37 +0330

    Make the split form a controlled form

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -155,21 +155,42 @@ function FormAddFriend({ onAddFriend }) {
 }
 
 function FormSplitBill({ selectedFriend }) {
+    const [bill, setBill] = useState("");
+    const [paidByUser, setPaidByUser] = useState("");
+    const paidByFriend = bill ? bill - paidByUser : "";
+    const [whoIsPaying, setWhoIsPaying] = useState("user");
+
     return (
         <form className="form-split-bill">
             <h2>Split a bill with {selectedFriend.name}</h2>
 
             <label>💰Bill value</label>
-            <input type="text" />
+            <input
+                type="text"
+                value={bill}
+                onChange={(e) => setBill(+e.target.value)}
+            />
 
             <label>🙋Your expense</label>
-            <input type="text" />
+            <input
+                type="text"
+                value={paidByUser}
+                // controls so the input on expense wouldn't get passed the bill value
+                onChange={(e) =>
+                    setPaidByUser(
+                        +e.target.value > bill ? paidByUser : +e.target.value
+                    )
+                }
+            />
 
             <label>👩🏻‍🤝‍👨🏻{selectedFriend.name}'s expense</label>
-            <input type="text" disabled />
+            <input type="text" value={paidByFriend} disabled />
 
             <label>💸Who is paying the bill? </label>
-            <select>
+            <select
+                value={whoIsPaying}
+                onChange={(e) => setWhoIsPaying(e.target.value)}
+            >
                 <option value="user">You</option>
                 <option value="friend">{selectedFriend.name}</option>
             </select>
