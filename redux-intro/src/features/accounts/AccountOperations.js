c97ae73dd5249216b7b4f2826521837369fdc754
Soheil <iamsoheil.r@gmail.com>
2025-09-20 02:10:59 +0330

    Apply redux devtools

--- redux-intro/src/features/accounts/AccountOperations.js
+++ redux-intro/src/features/accounts/AccountOperations.js
@@ -21,7 +21,7 @@ function AccountOperations() {
 
         dispatch(deposit(depositAmount, currency));
         setDepositAmount("");
-        setCurrency("");
+        setCurrency("USD");
     }
 
     function handleWithdrawal() {
