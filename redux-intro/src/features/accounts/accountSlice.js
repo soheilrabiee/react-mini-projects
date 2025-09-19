9dc0684a7fd8eb8d8732f1d93c165e9c913f584e
Soheil <iamsoheil.r@gmail.com>
2025-09-20 01:51:06 +0330

    Make the api call using thunk

--- redux-intro/src/features/accounts/accountSlice.js
+++ redux-intro/src/features/accounts/accountSlice.js
@@ -2,13 +2,18 @@ const initialStateAccount = {
     balance: 0,
     loan: 0,
     loanPurpose: "",
+    isLoading: false,
 };
 
 // Reducers can't have async code inside of them also they should not mutate states
 export default function accountReducer(state = initialStateAccount, action) {
     switch (action.type) {
         case "account/deposit":
-            return { ...state, balance: state.balance + action.payload };
+            return {
+                ...state,
+                balance: state.balance + action.payload,
+                isLoading: false,
+            };
         case "account/withdraw":
             return { ...state, balance: state.balance - action.payload };
         case "account/requestLoan":
@@ -26,14 +31,34 @@ export default function accountReducer(state = initialStateAccount, action) {
                 loanPurpose: "",
                 balance: state.balance - state.loan,
             };
+        case "account/convertingCurrency":
+            return {
+                ...state,
+                isLoading: true,
+            };
 
         default:
             return state;
     }
 }
 
-export function deposit(amount) {
-    return { type: "account/deposit", payload: amount };
+// Thunk middleware async function
+export function deposit(amount, currency) {
+    if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+    return async function (dispatch, getState) {
+        // Thunk functions can have multiple dispatches
+        dispatch({ type: "account/convertingCurrency" });
+        // API call
+        const res = await fetch(
+            `https://api.frankfurter.dev/v1/latest?base=${currency}&symbols=USD`
+        );
+        const data = await res.json();
+        const converted = (amount * data.rates.USD).toFixed(2);
+
+        // return action
+        dispatch({ type: "account/deposit", payload: +converted });
+    };
 }
 export function withdraw(amount) {
     return { type: "account/withdraw", payload: amount };
