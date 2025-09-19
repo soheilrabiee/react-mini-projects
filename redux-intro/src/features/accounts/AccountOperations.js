52cf55af0b0f2f20a081732478a10803c40569f9
Soheil <iamsoheil.r@gmail.com>
2025-09-19 23:31:01 +0330

    Handle account operations

--- redux-intro/src/features/accounts/AccountOperations.js
+++ redux-intro/src/features/accounts/AccountOperations.js
@@ -1,78 +1,109 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { deposit, payLoan, requestLoan, withdraw } from "./accountSlice";
 
 function AccountOperations() {
-  const [depositAmount, setDepositAmount] = useState("");
-  const [withdrawalAmount, setWithdrawalAmount] = useState("");
-  const [loanAmount, setLoanAmount] = useState("");
-  const [loanPurpose, setLoanPurpose] = useState("");
-  const [currency, setCurrency] = useState("USD");
+    const [depositAmount, setDepositAmount] = useState("");
+    const [withdrawalAmount, setWithdrawalAmount] = useState("");
+    const [loanAmount, setLoanAmount] = useState("");
+    const [loanPurpose, setLoanPurpose] = useState("");
+    const [currency, setCurrency] = useState("USD");
 
-  function handleDeposit() {}
+    const dispatch = useDispatch();
+    const { loan: currentLoan, loanPurpose: currentLoanPurpose } = useSelector(
+        (store) => store.account
+    );
 
-  function handleWithdrawal() {}
+    function handleDeposit() {
+        if (!depositAmount) return;
 
-  function handleRequestLoan() {}
+        dispatch(deposit(depositAmount));
+        setDepositAmount("");
+    }
 
-  function handlePayLoan() {}
+    function handleWithdrawal() {
+        if (!withdrawalAmount) return;
 
-  return (
-    <div>
-      <h2>Your account operations</h2>
-      <div className="inputs">
+        dispatch(withdraw(withdrawalAmount));
+        setWithdrawalAmount("");
+    }
+
+    function handleRequestLoan() {
+        if (!loanAmount || !loanPurpose) return;
+
+        dispatch(requestLoan(loanAmount, loanPurpose));
+        setLoanAmount("");
+        setLoanPurpose("");
+    }
+
+    function handlePayLoan() {
+        dispatch(payLoan());
+    }
+
+    return (
         <div>
-          <label>Deposit</label>
-          <input
-            type="number"
-            value={depositAmount}
-            onChange={(e) => setDepositAmount(+e.target.value)}
-          />
-          <select
-            value={currency}
-            onChange={(e) => setCurrency(e.target.value)}
-          >
-            <option value="USD">US Dollar</option>
-            <option value="EUR">Euro</option>
-            <option value="GBP">British Pound</option>
-          </select>
+            <h2>Your account operations</h2>
+            <div className="inputs">
+                <div>
+                    <label>Deposit</label>
+                    <input
+                        type="number"
+                        value={depositAmount}
+                        onChange={(e) => setDepositAmount(+e.target.value)}
+                    />
+                    <select
+                        value={currency}
+                        onChange={(e) => setCurrency(e.target.value)}
+                    >
+                        <option value="USD">US Dollar</option>
+                        <option value="EUR">Euro</option>
+                        <option value="GBP">British Pound</option>
+                    </select>
 
-          <button onClick={handleDeposit}>Deposit {depositAmount}</button>
-        </div>
+                    <button onClick={handleDeposit}>
+                        Deposit {depositAmount}
+                    </button>
+                </div>
 
-        <div>
-          <label>Withdraw</label>
-          <input
-            type="number"
-            value={withdrawalAmount}
-            onChange={(e) => setWithdrawalAmount(+e.target.value)}
-          />
-          <button onClick={handleWithdrawal}>
-            Withdraw {withdrawalAmount}
-          </button>
-        </div>
+                <div>
+                    <label>Withdraw</label>
+                    <input
+                        type="number"
+                        value={withdrawalAmount}
+                        onChange={(e) => setWithdrawalAmount(+e.target.value)}
+                    />
+                    <button onClick={handleWithdrawal}>
+                        Withdraw {withdrawalAmount}
+                    </button>
+                </div>
 
-        <div>
-          <label>Request loan</label>
-          <input
-            type="number"
-            value={loanAmount}
-            onChange={(e) => setLoanAmount(+e.target.value)}
-            placeholder="Loan amount"
-          />
-          <input
-            value={loanPurpose}
-            onChange={(e) => setLoanPurpose(e.target.value)}
-            placeholder="Loan purpose"
-          />
-          <button onClick={handleRequestLoan}>Request loan</button>
-        </div>
+                <div>
+                    <label>Request loan</label>
+                    <input
+                        type="number"
+                        value={loanAmount}
+                        onChange={(e) => setLoanAmount(+e.target.value)}
+                        placeholder="Loan amount"
+                    />
+                    <input
+                        value={loanPurpose}
+                        onChange={(e) => setLoanPurpose(e.target.value)}
+                        placeholder="Loan purpose"
+                    />
+                    <button onClick={handleRequestLoan}>Request loan</button>
+                </div>
 
-        <div>
-          <span>Pay back $X</span>
-          <button onClick={handlePayLoan}>Pay loan</button>
+                {currentLoan > 0 && (
+                    <div>
+                        <span>
+                            Pay back ${currentLoan}({currentLoanPurpose})
+                        </span>
+                        <button onClick={handlePayLoan}>Pay loan</button>
+                    </div>
+                )}
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default AccountOperations;
